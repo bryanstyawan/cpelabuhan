e28b7a27fab4696021e844ba4ef5fb69fa87e611
bryanstyawan <bryanstyawan@hotmail.com>
2019-07-17 23:46:48 +0700

    first commit

---
+++ application/modules/master/controllers/Activity.php
@@ -0,0 +1,100 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Activity extends CI_Controller {
+
+	public function __construct () {
+		parent::__construct();
+		// $this->load->model ('Mbank_data', '', TRUE);
+	}
+	
+	public function index()
+	{
+		$this->Globalrules->session_rule();						
+		$data['title']   = 'Activity';
+		$data['content'] = 'master/activity/index';
+		$data['list']    = $this->Allcrud->listData('mr_activity')->result_array();
+		$this->load->view('templateAdmin',$data);
+	}
+
+	public function store($arg=NULL,$oid=NULL)
+	{
+		# code...
+		$res_data    = 0;
+		$text_status = '';
+		$data_sender = array();
+		if ($arg == NULL) {
+			# code...
+			$data_sender = $this->input->post('data_sender');
+		}
+		else {
+			# code...
+			$data_sender['crud'] = $arg;
+			$data_sender['oid']  = $oid;
+		}
+
+		$data_store  = $this->Globalrules->trigger_insert_update($data_sender['crud']);
+		if ($data_sender['crud'] == 'insert') {
+			# code...
+			$data_store['name']            = $data_sender['f_name'];
+			            $res_data          = $this->Allcrud->addData('mr_activity',$data_store);
+			            $text_status       = $this->Globalrules->check_status_res($res_data,'Data activity berhasil ditambah.');						
+		} elseif ($data_sender['crud'] == 'update') {
+			# code...
+			$data_store['name']            = $data_sender['f_name'];
+			            $res_data          = $this->Allcrud->editData('mr_activity',$data_store,array('id'=>$data_sender['oid']));
+			            $text_status       = $this->Globalrules->check_status_res($res_data,'Data activity berhasil diupdate.');			
+		} elseif ($data_sender['crud'] == 'delete') {
+			# code...
+			$res_data          = $this->Allcrud->delData('mr_activity',array('id'=>$data_sender['oid']));
+			$text_status       = $this->Globalrules->check_status_res($res_data,'Data activity telah berhasil dihapus.');			
+		}
+
+		$res = array
+					(
+						'status' => $res_data,
+						'text'   => $text_status
+					);
+		echo json_encode($res);		
+	}
+
+	public function get_data($id,$arg=NULL,$table)
+	{
+		# code...
+		$data       = $this->Allcrud->getData($table,array('id'=>$id));		
+		if ($arg == 'ajax') {
+			# code...
+			$res_status = "";
+			$res_text   = "";
+			$res_data   = "";
+			if ($data->result_array() != array()) {
+				# code...
+				$res_data   = $data->result_array();	
+				$res_status = 1;
+				$res_text   = '';
+			}
+			else {
+				# code...
+				$res_data   = $data->result_array();
+				$res_status = $res_data;
+				$res_text   = 'Data tidak ditemukan';
+			}
+
+			$res = array
+						(
+							'status' => $res_status,
+							'data'   => $res_data,
+							'text'   => $res_text
+						);
+			echo json_encode($res);					
+		}
+		elseif($arg == 'result_array') {
+			# code...
+			return $data->result_array();
+		}
+		else {
+			# code...
+			return $data;			
+		}
+	}	
+}
---
+++ application/modules/master/views/product/index.php
@@ -0,0 +1,209 @@
+<section id="viewdata">
+	<div class="col-xs-12">
+		<div class="box">
+			<div class="box-header">
+				<h3 class="box-title"></h3>
+				<div class="box-tools pull-right"><button class="btn btn-block btn-primary" id="addData"><i class="fa fa-plus-square"></i> Tambah Data</button></div>
+			</div><!-- /.box-header -->
+			<div class="box-body" id="table_fill">
+				<table class="table table-bordered table-striped table-view">
+					<thead>
+				<tr>
+					<th>No</th>
+					<th>Nama</th>
+					<th>action</th>
+				</tr>
+				</thead>
+				<tbody>
+					<?php
+						if ($list != array()) {
+							# code...
+							for ($i=0; $i < count($list); $i++) { 
+								# code...
+					?>
+						<tr>
+							<td><?php echo $i+1;?></td>
+							<td><?php echo $list[$i]['name'];?></td>
+							<td>
+								<button class="btn btn-primary btn-xs" onclick="edit('<?php echo $list[$i]['id'];?>')"><i class="fa fa-edit"></i> Ubah</button>&nbsp;&nbsp;
+								<button class="btn btn-danger btn-xs" onclick="del('<?php echo $list[$i]['id'];?>')"><i class="fa fa-trash"></i> Hapus</button>
+							</td>
+						</tr>					
+					<?php
+							}
+						}
+					?>
+				</tbody>
+				</table>
+				
+			</div><!-- /.box-body -->
+			</div><!-- /.box -->
+	</div>
+</section>
+
+<section id="formdata" style="display:none">
+	<div class="col-xs-12">
+		<div class="box">
+			<div class="box-header">
+				<h3 class="box-title"></h3>
+				<div class="box-tools pull-right"><button class="btn btn-block btn-danger" id="closeData"><i class="fa fa-close"></i></button></div>				
+			</div>
+			<div class="box-body">
+				<div class="row">
+					<input class="form-control" type="hidden" id="oid">
+					<input class="form-control" type="hidden" id="crud">					
+					<div class="col-md-6">
+						<div class="form-group">
+							<label>Nama</label>
+							<input type="text" class="form-control" id="f_name" placeholder="Nama">
+						</div>
+					</div>
+				</div>
+
+			</div><!-- /.box-body -->
+			<div class="box-footer">
+				<a class="btn btn-success pull-right" id="btn-trigger-controll"><i class="fa fa-save"></i>&nbsp; Simpan</a>
+			</div>
+		</div><!-- /.box -->
+	</div>
+</section>
+<script>
+$(document).ready(function(){
+	$("#addData").click(function()
+	{
+		$(".form-control").val('');
+		$("#formdata").css({"display": ""})
+		$("#viewdata").css({"display": "none"})
+		$("#formdata > div > div > div.box-header > h3").html("Tambah Data");		
+		$("#crud").val('insert');
+		$("#section_file").css({"display": "none"})				
+	})
+
+	$("#closeData").click(function(){
+		$("#formdata").css({"display": "none"})
+		$("#viewdata").css({"display": ""})		
+	})	
+
+	$("#btn-trigger-controll").click(function(){
+		var res_status   = 0;
+		var flag_allowed = 0;
+		var oid          = $("#oid").val();
+		var crud         = $("#crud").val();
+		var f_name       = $("#f_name").val();
+
+		var data_sender = {
+			'oid'   : oid,
+			'crud'  : crud,
+			'f_name': f_name
+		}
+
+		if (crud == 'insert') {
+			flag_allowed = 1;				
+		}
+		else
+		{
+			flag_allowed = 1;
+		}
+
+		if (f_name.length <= 0) {
+			if (f_name.length <= 0) {
+				Lobibox.alert("warning", //AVAILABLE TYPES: "error", "info", "success", "warning"
+				{
+					title: 'Peringatan',					
+					msg: "Data Judul Buku belum terisi, mohon lengkapi data tersebut"
+				});				
+			}
+		}
+		else
+		{
+			if (flag_allowed == 1) {
+				$.ajax({
+					url :"<?php echo site_url();?>master/product/store",
+					type:"post",
+					data:{data_sender : data_sender},
+					beforeSend:function(){
+						$("#editData").modal('hide');
+						$("#loadprosess").modal('show');
+					},
+					success:function(msg){
+						var obj = jQuery.parseJSON (msg);
+						ajax_status(obj);
+					},
+					error:function(jqXHR,exception)
+					{
+						ajax_catch(jqXHR,exception);					
+					}
+				})					
+			}
+		}
+	})
+})
+
+function edit(id){
+	$.ajax({
+		url :"<?php echo site_url();?>master/product/get_data/"+id+"/ajax/mr_product",
+		type:"post",
+		beforeSend:function(){
+			$("#loadprosess").modal('show');
+		},
+		success:function(msg){
+			var obj = jQuery.parseJSON (msg);
+			if (obj.status == 1)
+			{
+				$(".form-control").val('');
+				$("#formdata").css({"display": ""})
+				$("#section_file").css({"display": ""})										
+				$("#viewdata").css({"display": "none"})
+				$("#formdata > div > div > div.box-header > h3").html("Ubah Data");		
+				$("#crud").val('update');
+				$("#oid").val(obj.data[0]['id']);
+				$("#f_name").val(obj.data[0]['name']);				
+				$("#loadprosess").modal('hide');				
+			}
+			else
+			{
+				Lobibox.notify('warning',{msg: obj.text});
+				setTimeout(function(){
+					$("#loadprosess").modal('hide');
+				}, 500);
+			}						
+		},
+		error:function(jqXHR,exception)
+		{
+			ajax_catch(jqXHR,exception);					
+		}
+	})
+}
+
+function del(id)
+{					
+	Lobibox.confirm({
+		title   : "Konfirmasi",
+		msg     : "Anda yakin akan menghapus data ini ?",
+		callback: function ($this, type) {
+			if (type === 'yes'){			
+				$.ajax({
+					url :"<?php echo site_url();?>master/product/store/"+'delete/'+id,
+					type:"post",
+					beforeSend:function(){
+						$("#editData").modal('hide');
+						$("#loadprosess").modal('show');
+					},
+					success:function(msg){
+						var obj = jQuery.parseJSON (msg);
+						ajax_status(obj);
+					},
+					error:function(jqXHR,exception)
+					{
+						ajax_catch(jqXHR,exception);					
+					}
+				})
+			}
+		}
+	})		
+}
+
+function detail(id) {
+	window.location.href = "<?php echo site_url();?>bank_data/soal/detail/"+id
+}
+</script>
